d3a5f0426bda14f4ca5ca9529e504ff8700796c7
Kyle Jones <kyle074613@gmail.com>
2020-01-04 10:49:01 -0800

    got the mapquest directions api call to work

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 var apiKey = "LnKRG2Lq3YrtUP9FxWVGpL57coUhpfWy";
+
+
+$("#submitButton").on("click", function () {
+    var startLocation = $("#startAddress").val();
+    var endLocation = $("#endAddress").val();
+    var mapquestUrl = "http://www.mapquestapi.com/directions/v2/route?key=" + apiKey + "&from=" + startLocation + "&to=" + endLocation;
+
+    $.ajax({
+        url: mapquestUrl,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+    });
+})
