e2b6722e8045c16ddd3695642db2c0f38387687e
Kyle Jones <kyle074613@gmail.com>
2020-01-07 20:06:08 -0800

    made function to populate restaurant list with media objects

--- script.js
+++ script.js
@@ -41,6 +41,10 @@ function createMap() {
     //map.addControl(L.mapquest.control());
 }
 
+function generateActivies() {
+
+}
+
 function generateRestaurants() {
     var yelpApiKey = "EKNdAx27IgYINE6TiVp9FhPB0Me3YrNSH44mLYiaKUp2XIvAVjBurD74d9e_GkjQtx_l2APPcgH3ZWEEDe7QMTL8iOqXmyShjPDdqEdSWiGa49JDB-Op7pTBeQIUXnYx";
 
@@ -53,8 +57,29 @@ function generateRestaurants() {
     }).then(function (response) {
         console.log(response)
 
+        for (var i = 0; i < numOfRestaurants; i++) {
+            var restaurantAddress;
+            var foodType;
+
+            console.log(response.businesses[i].location.display_address)
+            for (var j = 0; j < response.businesses[i].location.display_address.length; j++)
+                restaurantAddress += response.businesses[i].location.display_address[j] + ' ';
+
+            for (var k = 0; k < response.businesses[i].categories.length; k++)
+                foodType += response.businesses[i].categories[k].title + ' ';
 
 
+            var restaurantMediaObject = $('<div class="row"><div class="media"><img src="' + response.businesses[i].image_url + '"> ' +
+                '<div class="media-body"><h5 class="mt-0">' + response.businesses[i].name + '</h5>' +
+                '<p>Rating: ' + response.businesses[i].rating + '</p>' +
+                '<p>Address: ' + restaurantAddress + '</p>' +
+                '<p>Food Type: ' + foodType + '</p>' +
+                '<p>Phone Number: ' + response.businesses[i].display_phone + '</p>' +
+                '<p>URL: <a href=' + response.businesses[i].url + '>See ' + response.businesses[i].name + ' on Yelp!</a></p>')
+        }
+
+        console.log(restaurantMediaObject);
+        $("#restaurants").append(restaurantMediaObject);
     });
 }
 
@@ -65,8 +90,10 @@ $("#submitButton").on("click", function () {
     numOfRestaurants = $("#numRestaurants").val();
     numOfActivities = $("#numActivities").val();
 
-    console.log(numOfActivities + ' ' + numOfRestaurants)
-    createMap();
+    $("#restaurants").empty();
+    $("#activities").empty();
+
+    //createMap();
     generateRestaurants();
 
 })
