0d12d2ec59b3d1a21b37eee8fc70045ed97e6fe0
tnowls <tnowlan101@icloud.com>
2020-01-09 19:03:36 -0800

    parsley

--- script.js
+++ script.js
@@ -148,3 +148,13 @@ $("#submitButton").on("click", function () {
     generateRestaurants();
     generateActivies();
 })
+$(function () {
+$('#demo-form').parsley().on('field:validated', function() {
+    var ok = $('.parsley-error').length === 0;
+    $('.bs-callout-info').toggleClass('hidden', !ok);
+    $('.bs-callout-warning').toggleClass('hidden', ok);
+  })
+  .on('form:submitButton', function() {
+    return false; // Don't submit form for this demo
+  });
+});
